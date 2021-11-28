e6df0e0c170779d952ae42bae97b9e825c822aa1
Jacob Bashista <tgb.bashista@gmail.com>
2021-11-28 16:27:36 -0500

    Maybe this is better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from aiohttp import web
 import requests
-import time
+import asyncio
 
 HOST_NAME = "localhost"
 PORT = 4830
@@ -11,62 +11,78 @@ NODES_URL = BASE_URL + "/api/application/nodes"
 SERVERS_URL = BASE_URL + "/api/application/servers"
 LOCATIONS_URL = BASE_URL + "/api/application/locations?include=nodes,servers"
 PTERO_API_KEY = ""
+exporter_text = ""
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
+async def update_text():
+    headers = {'Authorization': 'Bearer ' + PTERO_API_KEY}
+    r = requests.get(USERS_URL, headers=headers)
+    response = r.json()
+    total_users = response["meta"]["pagination"]["total"]
 
-        if self.path == "/favicon.ico":
-            self.send_response(404)
-            return
-        headers = {'Authorization': 'Bearer ' + PTERO_API_KEY}
-        r = requests.get(USERS_URL, headers=headers)
-        response = r.json()
-        total_users = response["meta"]["pagination"]["total"]
-        time.sleep(0.1)
-        
-        r = requests.get(NODES_URL, headers=headers)
-        response = r.json()
-        total_nodes = response["meta"]["pagination"]["total"]
-        time.sleep(0.1)
+    r = requests.get(NODES_URL, headers=headers)
+    response = r.json()
+    total_nodes = response["meta"]["pagination"]["total"]
 
-        r = requests.get(SERVERS_URL, headers=headers)
-        response = r.json()
-        total_servers = response["meta"]["pagination"]["total"]
-        time.sleep(0.1)
+    r = requests.get(SERVERS_URL, headers=headers)
+    response = r.json()
+    total_servers = response["meta"]["pagination"]["total"]
 
-        r = requests.get(LOCATIONS_URL, headers=headers)
-        response = r.json()
-        total_locations = response["meta"]["pagination"]["total"]
-        time.sleep(0.1)
+    r = requests.get(LOCATIONS_URL, headers=headers)
+    response = r.json()
+    total_locations = response["meta"]["pagination"]["total"]
 
-        exporter_text = ""
-        exporter_text += f"# HELP total_users The total number of users registered in pterodactyl.\n# TYPE total_users gauge\ntotal_users {total_users}\n\n"
-        exporter_text += f"# HELP total_nodes The total number of nodes registered in pterodactyl.\n# TYPE total_nodes gauge\ntotal_nodes {total_nodes}\n\n"
-        exporter_text += f"# HELP total_servers The total number of servers registered in pterodactyl.\n# TYPE total_servers gauge\ntotal_servers {total_servers}\n\n"
-        exporter_text += f"# HELP total_locations The total number of locations registered in pterodactyl.\n# TYPE total_locations gauge\ntotal_locations {total_locations}\n\n"
+    global exporter_text
+    exporter_text = ""
+    exporter_text += f"# HELP total_users The total number of users registered in pterodactyl.\n# TYPE total_users gauge\ntotal_users {total_users}\n\n"
+    exporter_text += f"# HELP total_nodes The total number of nodes registered in pterodactyl.\n# TYPE total_nodes gauge\ntotal_nodes {total_nodes}\n\n"
+    exporter_text += f"# HELP total_servers The total number of servers registered in pterodactyl.\n# TYPE total_servers gauge\ntotal_servers {total_servers}\n\n"
+    exporter_text += f"# HELP total_locations The total number of locations registered in pterodactyl.\n# TYPE total_locations gauge\ntotal_locations {total_locations}\n\n"
 
-        for location in response["data"]:
-            name = location["attributes"]["short"].replace(" ", "_").replace("-", "_")
-            node_count = len(location["attributes"]["relationships"]["nodes"]["data"])
-            server_count = len(location["attributes"]["relationships"]["servers"]["data"])
-            exporter_text += f"# HELP {name}_nodes The total number of nodes at {name}.\n# TYPE {name}_nodes gauge\n{name}_nodes {node_count}\n\n"
-            exporter_text += f"# HELP {name}_servers The total number of servers at {name}.\n# TYPE {name}_servers gauge\n{name}_servers {server_count}\n\n"
+    for location in response["data"]:
+        name = location["attributes"]["short"].replace(" ", "_").replace("-", "_")
+        node_count = len(location["attributes"]["relationships"]["nodes"]["data"])
+        server_count = len(location["attributes"]["relationships"]["servers"]["data"])
+        exporter_text += f"# HELP {name}_nodes The total number of nodes at {name}.\n# TYPE {name}_nodes gauge\n{name}_nodes {node_count}\n\n"
+        exporter_text += f"# HELP {name}_servers The total number of servers at {name}.\n# TYPE {name}_servers gauge\n{name}_servers {server_count}\n\n"
+
+async def fetch_data():
+    while True:
+        await update_text()
+        print("Updated exporter")
+        await asyncio.sleep(300)
 
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(bytes(exporter_text, "utf-8"))
+async def print_test():
+    while True:
+        print('Test')
+        await asyncio.sleep(5)
+
+
+async def handle(request):
+    text = exporter_text
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/metrics', handle)])
+
+runners = []
+
+async def start_site(app, address='localhost', port=8080):
+    runner = web.AppRunner(app)
+    runners.append(runner)
+    await runner.setup()
+    site = web.TCPSite(runner, address, port)
+    await site.start()
 
 
 if __name__ == "__main__":
-    webServer = HTTPServer((HOST_NAME, PORT), MyServer)
-    print("Server started http://%s:%s" % (HOST_NAME, PORT))
-
+    loop = asyncio.get_event_loop()
+    loop.create_task(update_text())
+    loop.create_task(start_site(app, address=HOST_NAME, port=PORT))
+    loop.create_task(fetch_data())
     try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
+        loop.run_forever()
+    except:
         pass
-
-    webServer.server_close()
-    print("Server stopped.")
